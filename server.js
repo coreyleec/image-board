d6c5a02ce69afd72613812d2cccee90602de9695
Corey Lee <coreyleec91288@gmail.com>
2023-03-15 18:51:48 -0400

    change start script, add server.js in root. change backend query address

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 
+console.log("__dirname", __dirname, favicon, favicon.ico, /build/favicon.ico)
+
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
