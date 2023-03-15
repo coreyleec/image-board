be91e0f62220df8afe38f885039e5c4dc7104a3f
Corey Lee <coreyleec91288@gmail.com>
2023-03-15 04:54:44 -0400

    change start script, add server.js in root.

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
 app.get("*", (req, res) => {
   let url = path.join(__dirname, '../client/build', 'index.html');
   if (!url.startsWith('/app/')) // since we're on local windows
     url = url.substring(1);
   res.sendFile(url);
 });
+app.listen(port);
