4930a25c5518aa045da6123d52ef3816b3f968dc
Corey Lee <coreyleec91288@gmail.com>
2023-03-17 03:02:03 -0400

    testing server.js changes

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const favicon = require('express-favicon');
+// const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
+// app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 
 console.log("__dirname", __dirname, favicon, favicon.ico, app)
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
-  return res.send('pong');
-});
+// app.get('/ping', function (req, res) {
+//   return res.send('pong');
+// });
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -24,5 +24,6 @@ app.get("*", (req, res) => {
     url = url.substring(1);
   res.sendFile(url);
 });
+
 
 app.listen(port);
