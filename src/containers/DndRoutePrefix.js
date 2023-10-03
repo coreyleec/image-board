21f01b4f7d4deeed33bb68d55863034542886a8e
Corey Lee <coreyleec91288@gmail.com>
2023-10-03 01:36:34 -0400

    test dnd cont only

--- src/containers/DndRoutePrefix.js
+++ src/containers/DndRoutePrefix.js
@@ -37,8 +37,7 @@ return (
 
  <Route path={[ `${match.path}/folders/:id` , `${match.path}/favorites/:id` ]} 
       >
-        <div>
-         {props.mobile !== null && props.mobile && <MobilePhotoGrid
+        <DndContainer
             // subDirectory={subDirectory}
             mobile={props.mobile}
             loggedIn={props.loggedIn}
@@ -64,35 +63,7 @@ return (
               updateUserFavorites={props.updateUserFavorites}
               directory={props.directory}
               dbVersion={props.dbVersion}
-              /> }
-              {props.mobile !== null && !props.mobile && <DndContainer
-            // subDirectory={subDirectory}
-            mobile={props.mobile}
-            loggedIn={props.loggedIn}
-            folderCollaborators={props.folderCollaborators}
-              hightlighted={props.hightlighted}
-              setBaseName={props.setBaseName}
-              photos={props.photos}
-              colorArr={props.colorArr}
-              setPhotos={props.setPhotos}
-              openModal={props.openModal}
-              setOpenModal={props.setOpenModal}
-              folderShown={props.folderShown}
-              
-              uuid={props.uuid}
-              userId={props.userId}
-              currentUserId={props.currentUserId}
-              demo={props.demo}
-              setReorderedPhotos={props.setReorderedPhotos}
-              deletePhoto={props.deletePhoto}
-              enableDelete={props.enableDelete}
-              edit={props.edit}
-              reorderSubmit={props.reorderSubmit}
-              updateUserFavorites={props.updateUserFavorites}
-              directory={props.directory}
-              dbVersion={props.dbVersion}
-              /> }
-</div>
+              /> 
               </Route> 
 
 
